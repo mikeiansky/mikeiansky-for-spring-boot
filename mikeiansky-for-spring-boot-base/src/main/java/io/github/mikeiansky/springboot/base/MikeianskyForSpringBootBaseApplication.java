0f8068b312c96d97ce69a7fcc9395e24d1225a1d
mikeian <zhouwenxiang@ciwei.net>
2024-12-17 14:30:46 +0800

    condition test

--- mikeiansky-for-spring-boot-base/src/main/java/io/github/mikeiansky/springboot/base/MikeianskyForSpringBootBaseApplication.java
+++ mikeiansky-for-spring-boot-base/src/main/java/io/github/mikeiansky/springboot/base/MikeianskyForSpringBootBaseApplication.java
@@ -1,8 +1,6 @@
 package io.github.mikeiansky.springboot.base;
 
-import io.github.mikeiansky.springboot.base.component.ListAppComponent;
-import io.github.mikeiansky.springboot.base.component.MapAppComponent;
-import io.github.mikeiansky.springboot.base.component.SimpleAppComponent;
+import io.github.mikeiansky.springboot.base.configuration.ConditionalConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -12,12 +10,10 @@ public class MikeianskyForSpringBootBaseApplication {
 
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(MikeianskyForSpringBootBaseApplication.class, args);
-        SimpleAppComponent simpleAppComponent = context.getBean(SimpleAppComponent.class);
-        System.out.println(simpleAppComponent);
-        ListAppComponent listAppComponent = context.getBean(ListAppComponent.class);
-        System.out.println(listAppComponent);
-        MapAppComponent mapAppComponent = context.getBean(MapAppComponent.class);
-        System.out.println(mapAppComponent);
+
+        ConditionalConfiguration.MissClassComponent missClassComponent = context.getBean(ConditionalConfiguration.MissClassComponent.class);
+        System.out.println(missClassComponent);
+
     }
 
 }
---
+++ mikeiansky-for-spring-boot-base/src/main/java/io/github/mikeiansky/springboot/base/configuration/ConditionalConfiguration.java
@@ -0,0 +1,23 @@
+package io.github.mikeiansky.springboot.base.configuration;
+
+import io.github.mikeiansky.utils.CollKit;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author mike ian
+ * @date 2024/12/17
+ * @desc
+ **/
+@Configuration
+public class ConditionalConfiguration {
+
+    @ConditionalOnClass(value = CollKit.class)
+    @Component
+    public static class MissClassComponent {
+
+    }
+
+
+}
