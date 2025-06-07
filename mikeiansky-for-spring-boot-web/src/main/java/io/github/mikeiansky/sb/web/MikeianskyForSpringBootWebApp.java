233e2cb83dfb29e0edca432d262d124577cd4973
ian <929566854@qq.com>
2025-06-07 15:28:32 +0800

    Add spring boot web sample module;

---
+++ mikeiansky-for-spring-boot-web/src/main/java/io/github/mikeiansky/sb/web/MikeianskyForSpringBootWebApp.java
@@ -0,0 +1,13 @@
+package io.github.mikeiansky.sb.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MikeianskyForSpringBootWebApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MikeianskyForSpringBootWebApp.class, args);
+    }
+
+}
---
+++ mikeiansky-for-spring-boot-web/src/test/java/io/github/mikeiansky/sb/web/MikeianskyForSpringBootWebAppTests.java
@@ -0,0 +1,13 @@
+package io.github.mikeiansky.sb.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MikeianskyForSpringBootWebAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
